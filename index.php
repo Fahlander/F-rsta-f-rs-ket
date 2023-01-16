88c49a76468be4fa958968d3a4b10b7873bef6ca
Fahlander <liam.fahlander@gmail.com>
2023-01-16 09:31:57 +0100

    eh

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-h1 {
-  color: blue;
-}
-</style>
-</head>
 <body>
 
-<h1> <?php echo "Hello World!"; ?> </h1>
+<h1>My first php page</h1>
+
+<?php
+echo "Hello World!";
+?>
 
 </body>
 </html>
